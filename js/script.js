6381dafdfc5553762129d7ac1a6eb0f36c5acc17
MR DZMITRY MURZICH <montazhbel@mail.ru>
2023-12-26 20:17:17 +0300

    Project finished

--- js/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-let ulList = document.querySelectorAll(".nav-menu__list-item");
-console.log(ulList);
-let firstItem = ulList[0].innerHTML;
-console.log(firstItem);
-firstItem.style.color = `red`;
